2bde8db29af483a41a4dc1e8035ae421696d02ad
Scuritel <iaromashov@gmail.com>
2021-03-29 13:47:49 +0300

    Html page construct

---
+++ logic.js
@@ -0,0 +1,79 @@
+//myid=5beab55c-6521-454c-b39d-ea077399295d
+//https://api.faceit.com/stats/v1/stats/time/users/%userid/games/csgo?page=%page&size=%size
+//&page is optional
+//i17=Team Win
+
+//elo=Elo
+
+//m1=Matches
+//m2=Wins
+//m3=Kills
+//m4=Deaths
+//m5=Assists
+//m6=MVPs
+//m7=K/D Ratio
+//m8=Rounds
+//m9=Headshots
+//m10=Triple Kills
+//m11=Quadro Kills
+//m12=Penta Kills
+//m13=Total Headshots %
+//m14=K/R Ratio
+//m15=Total K/D Ratio
+//m16=Total K/R Ratio
+//m17=Total Headshots %
+//m18=Total Headshots per Match
+
+//k1=Average Kills
+//k2=Average Deaths
+//k3=Average Assists
+//k4=Average MVPs
+//k5=Average K/D Ratio
+//k6=Win Rate %
+//k7=Headshots per Match
+//k8=Average Headshots %
+//k9=Average K/R Ratio
+//k10=Average Triple Kills
+//k11=Average Quadro Kills
+//k12=Average Penta Kills
+//k13=Team Average K/D Ratio
+//k14=Team Average K/R Ratio
+//k15=Team Average Headshots %
+//k16=Team Headshots per Match
+
+//c1
+//c2=K/D Ratio
+//c3=K/R Ratio
+//c4=Headshots %
+//c5=Final Score
+//c6=Team K/D Ratio
+//c7=Team K/R Ratio
+//c8=Team Headshots %
+//c9=Team Headshots
+
+//s0=Recent Results
+//s1=Current Win Streak
+//s2=Longest Win Streak
+
+//i0=Region
+//i1=Map
+//i2=Winner
+//i3=First Half Score
+//i4=Second Half Score
+//i5=Team
+//i6=Kills
+//i7=Assists
+//i8=Deaths
+//i9=MVPs
+//i10=Result
+//i12=Rounds
+//i13=Headshots
+//i14=Triple Kills
+//i15=Quadro Kills
+//i16=Penta Kills
+//i18=Score
+//i19=Overtime score
+
+
+
+
